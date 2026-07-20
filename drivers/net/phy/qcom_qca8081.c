e5ab05dafe4511b4e450da7dfa8de71bdb4fc4c3
YYH2913 <yyh2913@qq.com>
2026-07-20 10:04:30 +0800

    net: phy: complete QCA8081 2.5G setup

--- drivers/net/phy/qcom_qca8081.c
+++ drivers/net/phy/qcom_qca8081.c
@@ -44,13 +44,122 @@
 #define QTI_8081_PHY_MMD3_AZ_TRAINING_CTRL       0x8008
 #define QTI_8081_PHY_MMD3_AZ_TRAINING_VAL        0x1c32
 
+#define QTI_8081_PHY_MMD1_MSE_THRESHOLD_20DB	0x8014
+#define QTI_8081_MSE_THRESHOLD_20DB_VALUE		0x0529
+#define QTI_8081_PHY_MMD1_MSE_THRESHOLD_17DB	0x800e
+#define QTI_8081_MSE_THRESHOLD_17DB_VALUE		0x0341
+#define QTI_8081_PHY_MMD1_MSE_THRESHOLD_27DB	0x801e
+#define QTI_8081_MSE_THRESHOLD_27DB_VALUE		0x0419
+#define QTI_8081_PHY_MMD1_MSE_THRESHOLD_28DB	0x8020
+#define QTI_8081_MSE_THRESHOLD_28DB_VALUE		0x0341
+
+#define QTI_8081_PHY_MMD7_TOP_OPTION1		0x901c
+#define QTI_8081_TOP_OPTION1_DATA		0x0000
+
+#define QTI_8081_PHY_MMD3_DEBUG_1		0xa100
+#define QTI_8081_MMD3_DEBUG_1_VALUE		0x9203
+#define QTI_8081_PHY_MMD3_DEBUG_2		0xa101
+#define QTI_8081_MMD3_DEBUG_2_VALUE		0x48ad
+#define QTI_8081_PHY_MMD3_DEBUG_3		0xa103
+#define QTI_8081_MMD3_DEBUG_3_VALUE		0x1698
+#define QTI_8081_PHY_MMD3_DEBUG_4		0xa105
+#define QTI_8081_MMD3_DEBUG_4_VALUE		0x8001
+#define QTI_8081_PHY_MMD3_DEBUG_5		0xa106
+#define QTI_8081_MMD3_DEBUG_5_VALUE		0x1111
+#define QTI_8081_PHY_MMD3_DEBUG_6		0xa011
+#define QTI_8081_MMD3_DEBUG_6_VALUE		0x5f85
+
+#define QTI_8081_PHY_MMD7_CHIP_TYPE		0x901d
+#define QTI_8081_PHY_CHIP_TYPE_1G		BIT(0)
+
+#define QTI_8081_AN_CTRL2			64
+#define QTI_8081_AN_THP_BP2_5GT		BIT(3)
+#define QTI_8081_AN_ADVFSRT2_5G		BIT(5)
+#define QTI_8081_PMA_10GBR_FSRT_CSR		147
+#define QTI_8081_PMA_10GBR_FSRT_ENABLE		BIT(0)
+
 #define QTI_8081_PHY_SERDES_ADDR_OFFSET		1
 #define QTI_8081_PHY_SERDES_MMD1_FIFO_CTRL	0x9072
 #define QTI_8081_PHY_FIFO_RSTN			BIT(11)
 
+static int qti_8081_fast_retrain_config(struct phy_device *phydev)
+{
+	static const struct {
+		int devad;
+		u16 reg;
+		u16 val;
+	} tuning[] = {
+		{ MDIO_MMD_AN, QTI_8081_PHY_MMD7_TOP_OPTION1,
+		  QTI_8081_TOP_OPTION1_DATA },
+		{ MDIO_MMD_PMAPMD, QTI_8081_PHY_MMD1_MSE_THRESHOLD_20DB,
+		  QTI_8081_MSE_THRESHOLD_20DB_VALUE },
+		{ MDIO_MMD_PMAPMD, QTI_8081_PHY_MMD1_MSE_THRESHOLD_17DB,
+		  QTI_8081_MSE_THRESHOLD_17DB_VALUE },
+		{ MDIO_MMD_PMAPMD, QTI_8081_PHY_MMD1_MSE_THRESHOLD_27DB,
+		  QTI_8081_MSE_THRESHOLD_27DB_VALUE },
+		{ MDIO_MMD_PMAPMD, QTI_8081_PHY_MMD1_MSE_THRESHOLD_28DB,
+		  QTI_8081_MSE_THRESHOLD_28DB_VALUE },
+		{ MDIO_MMD_PCS, QTI_8081_PHY_MMD3_DEBUG_1,
+		  QTI_8081_MMD3_DEBUG_1_VALUE },
+		{ MDIO_MMD_PCS, QTI_8081_PHY_MMD3_DEBUG_4,
+		  QTI_8081_MMD3_DEBUG_4_VALUE },
+		{ MDIO_MMD_PCS, QTI_8081_PHY_MMD3_DEBUG_5,
+		  QTI_8081_MMD3_DEBUG_5_VALUE },
+		{ MDIO_MMD_PCS, QTI_8081_PHY_MMD3_DEBUG_3,
+		  QTI_8081_MMD3_DEBUG_3_VALUE },
+		{ MDIO_MMD_PCS, QTI_8081_PHY_MMD3_DEBUG_6,
+		  QTI_8081_MMD3_DEBUG_6_VALUE },
+		{ MDIO_MMD_PCS, QTI_8081_PHY_MMD3_DEBUG_2,
+		  QTI_8081_MMD3_DEBUG_2_VALUE },
+	};
+	int i, ret;
+
+	ret = phy_modify_mmd(phydev, MDIO_MMD_AN, MDIO_AN_10GBT_CTRL,
+			     QTI_8081_AN_ADVFSRT2_5G,
+			     QTI_8081_AN_ADVFSRT2_5G);
+	if (ret)
+		return ret;
+
+	ret = phy_modify_mmd(phydev, MDIO_MMD_AN, QTI_8081_AN_CTRL2,
+			     QTI_8081_AN_THP_BP2_5GT,
+			     QTI_8081_AN_THP_BP2_5GT);
+	if (ret)
+		return ret;
+
+	ret = phy_modify_mmd(phydev, MDIO_MMD_PMAPMD,
+			     QTI_8081_PMA_10GBR_FSRT_CSR,
+			     QTI_8081_PMA_10GBR_FSRT_ENABLE,
+			     QTI_8081_PMA_10GBR_FSRT_ENABLE);
+	if (ret)
+		return ret;
+
+	for (i = 0; i < ARRAY_SIZE(tuning); i++) {
+		ret = phy_write_mmd(phydev, tuning[i].devad,
+				    tuning[i].reg, tuning[i].val);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
 static int qti_8081_config(struct phy_device *phydev)
 {
-	int adv_changed, ret;
+	int adv_changed, chip_type, ret;
+
+	chip_type = phy_read_mmd(phydev, MDIO_MMD_AN,
+				 QTI_8081_PHY_MMD7_CHIP_TYPE);
+	if (chip_type < 0)
+		return chip_type;
+
+	if (chip_type & QTI_8081_PHY_CHIP_TYPE_1G) {
+		phydev->supported &= ~SUPPORTED_2500baseX_Full;
+		phydev->advertising &= ~SUPPORTED_2500baseX_Full;
+	} else {
+		ret = qti_8081_fast_retrain_config(phydev);
+		if (ret)
+			return ret;
+	}
 
 	/* Enable vga when init napa to fix 8023az issue */
 	ret = phy_modify_mmd(phydev, QTI_8081_PHY_MMD3_NUM,
@@ -122,7 +231,8 @@ static int qti_8081_startup(struct phy_device *phydev)
 
 	ret = qti_8081_update_fifo(phydev);
 	if (ret)
-		return ret;
+		debug("QCA8081 PHY 0x%x SerDes FIFO update failed: %d\n",
+		      phydev->addr, ret);
 
 	if (phy_data & QTI_8081_STATUS_FULL_DUPLEX)
 		phydev->duplex = DUPLEX_FULL;
